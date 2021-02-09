7ba88001028e52a168fa8547fa1603d30b2c8875
Vladimir Avakumov <avakdev@gmail.com>
2021-02-09 19:51:10 +0400

    added stylelint

--- fullscreen-slider/gulpfile.js
+++ fullscreen-slider/gulpfile.js
@@ -1,22 +1,35 @@
-const gulp = require("gulp")
-const gulpSass = require("gulp-sass")
-const browserSync = require("browser-sync").create()
-
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const gulpStylelint = require("gulp-stylelint");
 
 function style() {
-    return gulp.src("./scss/**/*.scss").pipe(gulpSass()).pipe(gulp.dest("./css")).pipe(browserSync.stream())
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(gulpSass())
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    })
-    gulp.watch("./scss/**/*.scss", style)
-    gulp.watch("./*.html").on("change", browserSync.reload)
-    gulp.watch("./**/*.js").on("change", browserSync.reload)
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./**/*.js").on("change", browserSync.reload);
 }
 
-exports.style = style
-exports.watch = watch
+function lintCss() {
+  return gulp.src("./scss/**/*.scss").pipe(
+    gulpStylelint({
+      reporters: [{ formatter: "string", console: true }],
+    })
+  );
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.lintCss = lintCss;
